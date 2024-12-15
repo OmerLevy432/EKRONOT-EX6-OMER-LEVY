69792907fee64dcb20b9c3f126bc24bfadf0864d
OmerLevy432 <olevy34@gmail.com>
2024-12-15 19:21:41 +0200

    done

---
+++ Password/Password.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct Password
+{
+	char value[16];
+	bool incorrect;
+	Password() : value(""), incorrect(true)
+	{
+	}
+};
+
+int main()
+{
+	std::cout << "Enter your password to continue:" << std::endl;
+	Password pwd;
+	std::cin >> pwd.value;
+
+	if (!strcmp(pwd.value, "********"))
+		pwd.incorrect = false;
+
+	if(!pwd.incorrect)
+		std::cout << "Congratulations\n";
+
+	// you can enter any password at length of 16 characters because its the size of the value buffer
+	// then the last key which is null returns and ! of null is 1
+
+	return 0;
+}
